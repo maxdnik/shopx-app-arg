f2fb4392366b4880c3b2abad3e38d23dc4b0a730
Maxi Dimnik <maxidimnik@gmail.com>
2026-06-29 17:05:43 -0300

    Use correct Google iOS OAuth client

--- lib/google-auth-config.ts
+++ lib/google-auth-config.ts
@@ -6,16 +6,20 @@ const GOOGLE_WEB_CLIENT_ID =
 
 const GOOGLE_IOS_CLIENT_ID =
   process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID ||
-  "935352834479-tf3habem4sk9nl3ut82c9b1ocvjt7t8k.apps.googleusercontent.com";
+  "935352834479-alqde8iks44c9l892mm1nmrrmt65vdgqc.apps.googleusercontent.com";
 
 const GOOGLE_ANDROID_CLIENT_ID =
   process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID || "";
+
+const GOOGLE_IOS_REVERSE_CLIENT_ID =
+  "com.googleusercontent.apps." +
+  GOOGLE_IOS_CLIENT_ID.replace(".apps.googleusercontent.com", "");
 
 export const GOOGLE_AUTH_CONFIG = {
   webClientId: GOOGLE_WEB_CLIENT_ID,
   iosClientId: GOOGLE_IOS_CLIENT_ID,
   androidClientId: GOOGLE_ANDROID_CLIENT_ID,
   redirectScheme: "shopxapp",
-  iosReverseClientId: "com.googleusercontent.apps.935352834479-tf3habem4sk9nl3ut82c9b1ocvjt7t8k",
-  iosRedirectUri: "com.googleusercontent.apps.935352834479-tf3habem4sk9nl3ut82c9b1ocvjt7t8k:/oauthredirect",
+  iosReverseClientId: GOOGLE_IOS_REVERSE_CLIENT_ID,
+  iosRedirectUri: `${GOOGLE_IOS_REVERSE_CLIENT_ID}:/oauth2redirect/google`,
 };
